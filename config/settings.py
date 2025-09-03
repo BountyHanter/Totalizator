5a0f6f3ef282429fed5466600d9da6b68c5633ca
denis <denison99@mail.ru>
2025-09-03 17:09:40 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -106,8 +106,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     # 'EXCEPTION_HANDLER': 'config.utils.exceptions.custom_exception_handler',
-    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    # "PAGE_SIZE": 100,  # сколько объектов возвращать на страницу по умолчанию
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,  # сколько объектов возвращать на страницу по умолчанию
     "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%S.%f%z",
     "USE_TZ": True,
 }
