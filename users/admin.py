17d830d332e758dfd940702caef35bb7d84209ac
denis <denison99@mail.ru>
2025-09-05 18:04:38 +0500

    fix admin color

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from users.models import CustomUser
+from users.models import CustomUser, ColorInterval
 
 
 @admin.register(CustomUser)
@@ -11,3 +11,10 @@ class CustomUserAdmin(UserAdmin):
         ('Дополнительно', {'fields': ('balance_cached',)}),
     )
     readonly_fields = ('balance_cached',)
+
+
+@admin.register(ColorInterval)
+class ColorIntervalAdmin(admin.ModelAdmin):
+    list_display = ("start_value", "end_value", "background_color", "border_color")
+    list_filter = ("background_color", "border_color")
+    search_fields = ("start_value", "end_value", "background_color", "border_color")
