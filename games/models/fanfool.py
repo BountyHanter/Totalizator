977f21a53cce3b5ed0c784cdb4a8a800c5d7f59f
denis <denison99@mail.ru>
2025-10-19 17:28:28 +0500

    fix

--- games/models/fanfool.py
+++ games/models/fanfool.py
@@ -11,8 +11,5 @@ class FanPool(SingletonModel):
     rounds_interval = models.PositiveIntegerField(default=1000, help_text="Каждые N раундов разыгрывать фонд")
     amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, help_text="Текущая сумма фонда")
 
-    rounds_since_distribution = models.PositiveIntegerField(default=0, verbose_name="Раундов прошло с розыгрыша")
-    updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return f"FanPool: {self.amount} (каждые {self.rounds_interval} раундов)"
